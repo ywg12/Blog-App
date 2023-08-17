edd8be68bc77537e194eb8b5d85b8a5d915d2ea4
Yashwardhan Gaur <yashwardhangaur1204@gmail.com>
2023-08-17 12:51:15 +0530

    Class Based View & Custom Permission

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -7,9 +7,14 @@ from .views import *
 router = routers.DefaultRouter()
 router.register(r'postapi', PostsViewSet) 
 urlpatterns = [
-    path("home", views.starting_page, name= "starting-page"),
-    path("posts", views.posts, name= "posts-page"),
-    path("posts/<slug:slug>", views.post_detail, name= "post-detail-page"), #/posts/my-first-post
+    #function-based calls
+    # path("home", views.starting_page, name= "starting-page"),
+    # path("posts", views.posts, name= "posts-page"),
+    # path("posts/<slug:slug>", views.post_detail, name= "post-detail-page"), #/posts/my-first-post
+    #class-based calls
+    path("home/", StartingPageView.as_view(), name="starting-page"),
+    path("posts/", PostsView.as_view(), name="posts-page"),
+    path("posts/<slug:slug>/", PostDetailView.as_view(), name="post-detail-page"),  # /posts/my-first-post
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
     path('search/', PostsViewSet.as_view({'get': 'search_by_title'}), name='search-posts-by-title'),
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render,get_object_or_404
+from django.views import View
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from .serializers import PostsSerializer
 from .models import Post
 from rest_framework.permissions import *
-from rest_framework import permissions
+from rest_framework import permissions,viewsets
 
 
 # Create your views here.
-@permission_required('blog.view_post')
+# Function based views
+'''@permission_required('blog.view_post')
 def starting_page(request):
     latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html",{
@@ -33,20 +36,55 @@ def post_detail(request, slug):
         "post": identified_post,
         "post_tags": identified_post.tags.all()
     })
+'''
 
 
 class HasSpecificPermission(permissions.BasePermission):
+    def __init__(self, permission_required):
+        self.permission_required = permission_required
+    
     def has_permission(self, request, view):
-        # Customize this logic based on your requirements
-        # For example, check if the user has a specific permission
-        return request.user.has_perm('blog.view_post')
+        return request.user.has_perm(self.permission_required)    
+
+
+class StartingPageView(PermissionRequiredMixin, View):
+    permission_required = ['blog.view_post']  # Default required permission
+
+    def get(self, request):
+        latest_posts = Post.objects.all().order_by("-date")[:3]
+        return render(request, "blog/index.html", {
+            "posts": latest_posts
+        })
+
+    def get_permission_required(self):
+        return self.permission_required
+
+class PostsView(PermissionRequiredMixin, View):
+    permission_required = ['blog.view_post']
+    def get(self, request):
+        all_posts = Post.objects.all().order_by("-date")
+        return render(request, "blog/all-posts.html", {
+            "all_posts": all_posts
+        })
+
+class PostDetailView(PermissionRequiredMixin, View):
+    permission_required = ['blog.view_author']
+    def get(self, request, slug):
+        identified_post = get_object_or_404(Post, slug=slug)
+        return render(request, "blog/post-detail.html", {
+            "post": identified_post,
+            "post_tags": identified_post.tags.all()
+        })
 
 
 class PostsViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostsSerializer
-    permission_classes = [HasSpecificPermission]
 
+    def get_permissions(self):
+        permission_classes = [HasSpecificPermission('blog.view_post')]
+        return permission_classes
+    
     @swagger_auto_schema(
         manual_parameters=[
             openapi.Parameter(
@@ -64,3 +102,5 @@ class PostsViewSet(viewsets.ModelViewSet):
         posts = Post.objects.filter(title__icontains=title_query)
         serializer = self.get_serializer(posts, many=True)
         return Response(serializer.data)
+
+
