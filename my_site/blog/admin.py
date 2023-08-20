c9f6b7e7524cf3c96e2d324705dc51a5d34b2575
Yashwardhan Gaur <yashwardhangaur1204@gmail.com>
2023-08-20 17:23:38 +0530

    Implemented Multi Image Upload

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,6 +1,6 @@
 import os
 from django.contrib import admin
-from .models import Post, Author, Tag, UserProfile
+from .models import Post, Author, Tag, UserProfile,UserImage
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 # Register your models here.
@@ -10,29 +10,34 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ("title", "date", "author")
     prepopulated_fields = {"slug": ("title",)}
     
-class ImageAdmin(admin.ModelAdmin):
-    list_display = ['user', 'image']
+# class ImageAdmin(admin.ModelAdmin):
+#     list_display = ['user', 'image']
     
-    def save_model(self, request, obj, form, change):
-        # Delete the old image if it exists
-        if obj.id:
-            old_obj = UserProfile.objects.get(id=obj.id)
-            if old_obj.image and obj.image != old_obj.image:
-                old_obj.delete()
+#     def save_model(self, request, obj, form, change):
+#         # Delete the old image if it exists
+#         if obj.id:
+#             old_obj = UserProfile.objects.get(id=obj.id)
+#             if old_obj.image and obj.image != old_obj.image:
+#                 old_obj.delete()
         
-        # Save the new image
-        super().save_model(request, obj, form, change)
+#         # Save the new image
+#         super().save_model(request, obj, form, change)
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'get_user_images']
+    
+    def get_user_images(self, obj):
+        return ", ".join([str(image) for image in obj.userimage_set.all()])
+    get_user_images.short_description = 'User Images'
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(UserImage)  # Register UserImage here
+
         
 
         
 admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
-admin.site.register(UserProfile, ImageAdmin)
-
-@receiver(pre_delete, sender=UserProfile)
-def delete_user_profile_image(sender, instance, **kwargs):
-    # Delete the image file from the media folder when the UserProfile instance is deleted
-    if instance.image:
-        instance.image.delete(save=False)
+# admin.site.register(UserProfile, ImageAdmin)
     
